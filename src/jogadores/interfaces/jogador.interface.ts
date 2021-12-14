d9aa51e4291aa09a65c8c280783cac1d02b8a20f
felipeduartedeveloper <felipesilvapac@gmail.com>
2021-12-14 16:16:53 -0300

    create interface jogador

---
+++ src/jogadores/interfaces/jogador.interface.ts
@@ -0,0 +1,9 @@
+export interface Jogador {
+  readonly _id: string;
+  readonly telefone: string;
+  readonly email: string;
+  nome: string;
+  ranking: string;
+  pocicaoRanking: number;
+  urlFotoJogador: string;
+}
